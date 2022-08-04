2e8fda28219c343ec8bd8f596887a49e443b3675
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-08-04 17:29:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.right.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left < 0) {
+      dodger.style.right = `${left + 1}px`;
+    }
+  }
+  
